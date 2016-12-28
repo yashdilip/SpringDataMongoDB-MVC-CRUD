a64ad8b14d121d3ad0db0a4357dcf73310df91e2
Dilip <Dilip@DESKTOP-5Q8N9DO>
2016-12-27 21:57:02 -0800

    Spring Data MongoDB dependency and enabling Mongo repository using @EnableMongoRepositories annotation

--- src/main/java/com/mongodb/crud/SprigDataMongoDbcrudApplication.java
+++ src/main/java/com/mongodb/crud/SprigDataMongoDbcrudApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-/*@EnableMongoRepositories("com.mongodb.crud.repositories")*/
+@EnableMongoRepositories("com.mongodb.crud.repositories")
 public class SprigDataMongoDbcrudApplication{
 
 	/*@Override
